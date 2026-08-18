b31ff59af49bff3ad079e3c4ec77448cbe6c81dd
Gizemnur Avcı <gizemnuravci4@gmail.com>
2026-08-18 12:28:58 +0300

    Update routing configurations and deployment settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.jsx';
-import { store, persistor } from './redux/store.js';
+import { store } from './redux/store.js';
 import ErrorBoundary from './components/ErrorBoundary.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
+        <App />
       </Provider>
     </ErrorBoundary>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,47 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/authSlice";
 import { nanniesReducer } from "./nannies/nanniesSlice";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-
-// Vite 8 / rolldown ile redux-persist/lib/storage uyumsuz olduğu için
-// localStorage'ı doğrudan saran custom storage kullanıyoruz
-const storage = {
-  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
-  setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
-  removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
-};
-
-
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["nannies"],
-};
 
 const rootReducer = combineReducers({
   auth: authReducer,
   nannies: nanniesReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
